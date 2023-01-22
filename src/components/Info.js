89ff4d984d1f87e332ac912f043ca40a52d435b0
AliAksoyy <alifeyza4017@gmail.com>
2023-01-22 16:05:17 +0300

    v8

--- src/components/Info.js
+++ src/components/Info.js
@@ -17,8 +17,24 @@ const UserInfo = () => {
 
 
 
-  return <h2>user info component </h2>;
+  return <section>
+    <Wrapper className='section-center'>
+      {items.map((item)=> {
+        return <Item key={item.id} {...item}></Item>
+      })}
+    </Wrapper>
+  </section>;
 };
+
+const Item =({icon,label,value,color})=> {
+  return <article className='item'>
+    <span className={color}>{icon}</span>
+    <div>
+      <h3>{value}</h3>
+      <p>{label}</p>
+    </div>
+  </article>
+}
 
 const Wrapper = styled.section`
   display: grid;
