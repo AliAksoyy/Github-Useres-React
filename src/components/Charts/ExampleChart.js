10755b32a4dab9e52438f2eaa6a4208eee66503c
AliAksoyy <alifeyza4017@gmail.com>
2023-01-23 20:17:37 +0300

    v12

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -1,32 +1,23 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React, { useContext } from "react";
-
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Column2D from "fusioncharts/fusioncharts.charts";
-
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 import { GithubContext } from "../../context/context";
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
-
-// STEP 2 - Chart Data
-const chartData =repos()
-
-
-// STEP 3 - Creating the JSON object to store the chart configurations
+const ChartComponent = () => {
+  const {repos}=useContext(GithubContext)
+  console.log(repos)
+ const chartData=repos.map((item)=> {
+  return {
+    label:"ali",
+    value:"beyda"
+  }
+ })
 const chartConfigs = {
   type: "column2d", // The chart type
-  width: "700", // Width of the chart
+  width: "400", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
   dataSource: {
@@ -50,9 +41,8 @@ const chartConfigs = {
 };
 
 
-const ChartComponent = () => {
-const {repos}=useContext(GithubContext)
-console.log(repos)
+
+
   return (<ReactFC {...chartConfigs} />);
 }
 
