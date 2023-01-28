b80c7f343f22f1e9039f78ca033816444e24d9c5
AliAksoyy <alifeyza4017@gmail.com>
2023-01-28 23:06:58 +0300

    v33

--- src/context/context.js
+++ src/context/context.js
@@ -22,13 +22,11 @@ const GithubProvider =({children})=> {
         toggleError()
         setLoading(true)
          const {data}= await axios(`${rootUrl}/users/${user}`).catch(err=>console.log(err))
-         const ali=await axios(`${rootUrl}/users/${user}/followers`).catch(err=>console.log(err))
-         const repos=await axios(`${rootUrl}/users/${user}/repos?per_page=100`).catch(err=>console.log(err))
-         setFollowers(ali.data)
-         console.log(repos);
-         setRepos(repos.data)
+        
         if(data){
             setGithubUser(data)
+           await axios(`${rootUrl}/users/${user}/followers`).then(res=>setFollowers(res.data)).catch(err=>console.log(err))
+           await axios(`${rootUrl}/users/${user}/repos?per_page=100`).then((res)=>setRepos(res.data)).catch(err=>console.log(err))
         }else{
             toggleError(true,"there is no user with that username")
         }
