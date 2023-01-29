c75b6a2e853d9200ccc0a4afb576f38264c70670
AliAksoyy <alifeyza4017@gmail.com>
2023-01-29 09:51:56 +0300

    v35

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from './App';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-// dev-7ukt26sro2rg6xu2.us.auth0.com
-// SzxMuEeBLZkvyd7UuvC3QHiijpRERSYp
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<GithubProvider>
-    <App />
-</GithubProvider>
+    <Auth0Provider
+    domain="dev-7ukt26sro2rg6xu2.us.auth0.com"
+    clientId="SzxMuEeBLZkvyd7UuvC3QHiijpRERSYp"
+      redirect_uri={window.location.origin}
+    
+    >
+        <GithubProvider>
+            <App />
+        </GithubProvider>
+    </Auth0Provider>
 );
 
 
