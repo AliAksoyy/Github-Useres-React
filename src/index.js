3fc33ff1a08b05fd08fd267e1292b9122cbe93f4
AliAksoyy <alifeyza4017@gmail.com>
2023-01-29 14:58:05 +0300

    v35

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Auth0Provider
-    domain="dev-7ukt26sro2rg6xu2.us.auth0.com"
-    clientId="SzxMuEeBLZkvyd7UuvC3QHiijpRERSYp"
-      redirect_uri={window.location.origin}
-    
-    >
+    domain='dev-7ukt26sro2rg6xu2.us.auth0.com'
+    clientId='SzxMuEeBLZkvyd7UuvC3QHiijpRERSYp'
+    redirectUri={window.location.origin}>
         <GithubProvider>
             <App />
         </GithubProvider>
