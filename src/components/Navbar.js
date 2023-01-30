ccd90eee1126168efa615d4c3c6f2095666c5df4
AliAksoyy <alifeyza4017@gmail.com>
2023-01-30 08:49:46 +0300

    v41

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
 
-  const {isAuthenticated,loginWithRedirect,logout,user,isLoading}=useAuth0()
+  const {isAuthenticated,loginWithRedirect,logout,user}=useAuth0()
   const isUser= isAuthenticated && user;
 
   return <Wrapper>
